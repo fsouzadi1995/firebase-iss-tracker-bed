780a5bdf6cf0125653b2a348e5b4a5dde0c156bf
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-05-19 22:00:15 -0300

    chore: debug heroku vars

--- app.ts
+++ app.ts
@@ -13,6 +13,15 @@ import 'firebase/firestore';
 
 import indexRouter from './routes/index';
 
+console.warn({
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
+});
+
 const fbase = firebase.initializeApp({
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
