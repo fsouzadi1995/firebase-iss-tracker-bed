1d7dcddd85c63737550fee41be35d752e8609ffd
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-05-19 22:05:53 -0300

    chore: debugging heroku

--- app.ts
+++ app.ts
@@ -13,6 +13,18 @@ import 'firebase/firestore';
 
 import indexRouter from './routes/index';
 
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 console.warn({
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -35,25 +47,15 @@ const db = fbase.firestore();
 
 setInterval(() => {
   fetch('http://api.open-notify.org/iss-now.json').then(async (res) => {
+    console.log('inside interval!');
+
     const location = await res.json();
 
     const doc = db.collection('locations').doc(String(location.timestamp));
 
     doc.set(location);
   });
-}, 30000);
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+}, 5000);
 
 app.use('/', indexRouter);
 
