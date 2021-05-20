92be220c188b70ba0203dae047e2959befc6ade3
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-05-19 21:44:41 -0300

    build: add env keys

--- app.ts
+++ app.ts
@@ -13,7 +13,14 @@ import 'firebase/firestore';
 
 import indexRouter from './routes/index';
 
-const fbase = firebase.initializeApp({});
+const fbase = firebase.initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
+});
 
 const db = fbase.firestore();
 
